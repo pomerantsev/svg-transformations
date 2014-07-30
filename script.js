af81d87c2e7724f398c1c38464efa0fd7e11a1c0
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-07-30 23:08:47 +0400

    Transition between two colors when scrolling

--- script.js
+++ script.js
@@ -5,7 +5,19 @@
   angular.module('app', [])
     .controller('MainController', function ($scope, $window, $document) {
     })
-    .directive('ppInverseScrolling', function ($window, $document) {
+    .factory('scroll', function ($window, $document) {
+      return {
+        getRelativePosition: function () {
+          var scrollHeight = $document[0].body.offsetHeight - $window.innerHeight;
+          if (scrollHeight > 0) {
+            return $window.pageYOffset / scrollHeight;
+          } else {
+            return 0;
+          }
+        }
+      };
+    })
+    .directive('ppInverseScrolling', function ($window, $document, scroll) {
       return {
         restrict: 'E',
         replace: true,
@@ -13,19 +25,10 @@
         scope: {},
         template: '<div ng-transclude ng-style="{transform: \'translateY(\' + yTranslate + \'px)\'}"></div>',
         link: function (scope, element) {
-          function getRelativePosition () {
-            var scrollHeight = $document[0].body.offsetHeight - $window.innerHeight;
-            if (scrollHeight > 0) {
-              return $window.pageYOffset / scrollHeight;
-            } else {
-              return 0;
-            }
-          }
-
           function getYTranslate () {
             var scrollHeight = $window.innerHeight - element[0].offsetHeight;
             if (scrollHeight > 0) {
-              return Math.min($window.pageYOffset + scrollHeight * getRelativePosition(), $document[0].body.offsetHeight - element[0].offsetHeight);
+              return Math.min($window.pageYOffset + scrollHeight * scroll.getRelativePosition(), $document[0].body.offsetHeight - element[0].offsetHeight);
             } else {
               return 0;
             }
@@ -38,6 +41,57 @@
               scope.yTranslate = getYTranslate();
             });
           });
+        }
+      };
+    })
+    .directive('ppIridescent', function ($window, scroll) {
+      return {
+        link: function (scope, element, attrs) {
+          function getRandomColor() {
+            return Math.floor(Math.random() * 256);
+          }
+
+          function setColor (r, g, b) {
+            function getFullColor () {
+              return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+            }
+            element.css({
+              stroke: getFullColor(),
+              fill: getFullColor()
+            });
+          }
+
+          function getIntermediateColor (initial, final, relative) {
+            return Math.round(initial + (final - initial) * relative);
+          }
+
+          var previousScrollDirection = 1,
+              initialRed = getRandomColor(),
+              initialGreen = getRandomColor(),
+              initialBlue = getRandomColor(),
+              initialRelativePosition = previousRelativePosition = scroll.getRelativePosition(),
+              finalRelativePosition = 1,
+              finalRed = getRandomColor(),
+              finalGreen = getRandomColor(),
+              finalBlue = getRandomColor();
+
+          angular.element($window).on('scroll', function () {
+            var currentRelativePosition = scroll.getRelativePosition(),
+                currentScrollDirection = (currentRelativePosition - previousRelativePosition > 0 ? 1 : -1);
+            if (currentScrollDirection === previousScrollDirection) {
+              var colorRelativePosition = (currentRelativePosition - initialRelativePosition) / (finalRelativePosition - initialRelativePosition),
+                  currentRed = getIntermediateColor(initialRed, finalRed, colorRelativePosition),
+                  currentGreen = getIntermediateColor(initialGreen, finalGreen, colorRelativePosition),
+                  currentBlue = getIntermediateColor(initialBlue, finalBlue, colorRelativePosition);
+              setColor(currentRed, currentGreen, currentBlue);
+            } else {
+
+            }
+            previousRelativePosition = currentRelativePosition;
+            previousScrollDirection = currentScrollDirection;
+          });
+
+          setColor(initialRed, initialGreen, initialBlue);
         }
       };
     });
